4e1098eed692ba62001be6b59a4a368fc1726987
Sachin <sachin_rcz@yahoo.co.in>
2018-12-02 20:25:16 +0530

    artistang

---
+++ app/settings.py
@@ -0,0 +1,5 @@
+BASE_DIR = 'data/'
+BASE_DOMAIN = 'https://www.artisantg.com'
+OUT_CSVFILENAME = 'Artistang_Data_1.csv'
+IN_CSVFILENAME = 'Artisian Ecotech.csv'
+
---
+++ crawler/artistang_com.py
@@ -0,0 +1,59 @@
+import logging
+from app.base import Basic_Crawler
+import re
+import os
+import pandas as pd
+from app import settings
+import csv
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+class Artistang_Com(Basic_Crawler):
+
+    def __init__(self, base_dir = settings.BASE_DIR, csvfilename=settings.OUT_CSVFILENAME, base_domain= settings.BASE_DOMAIN, inputfilename=settings.IN_CSVFILENAME):
+        self.base_domain = base_domain
+        self.base_dir = base_dir
+        self.csvfilename = csvfilename
+        self.inputfilename = inputfilename
+        logger.debug('Test')
+
+    def read_csv(self):
+        filename = os.path.join(self.base_dir, self.inputfilename)
+        logger.debug('Reading: {}'.format(filename))
+        with open(filename) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                product_name = row['Product_Name']
+                product_url = row['Product_URL']
+                self.extract_details(product_name, product_url)
+
+    def extract_details(self, product_name, product_url):
+        soup = self.get_soup(product_url)
+        headers = ['Product_Name', 'Manufacturer','Description','Features','Stock_Number','Product_URL']
+        mfg = soup.find('a', attrs={'href':re.compile('/Mfgr/')})
+        if mfg:
+            mfg = mfg.text
+        description = soup.select_one('div.normaltext')
+        if description:
+            description = description.text
+        features = soup.select_one('div.cpfeatures')
+        if features:
+            features = features.text.strip(' Features  ')
+        stocknum = soup.select_one('span.sti')
+        if stocknum:
+            stocknum = stocknum.text
+
+        data ={
+            'Product_Name': product_name,
+            'Manufacturer': mfg,
+            'Description': description,
+            'Features': features,
+            'Stock_Number':stocknum,
+            'Product_URL':product_url,
+        }
+        self.save_to_csv(data=data, base_dir=self.base_dir, headers=headers, filename=self.csvfilename)
+
+if __name__ == '__main__':
+    app = Artistang_Com()
+    app.read_csv()
